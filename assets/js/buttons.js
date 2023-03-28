335593374d2cc5984204cbfce83ed48523dfa568
heymendezz <gabriel@mendezz.net>
2023-03-28 10:05:53 -0300

    Refatoração codigo 1/3

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,24 +1,29 @@
 let projects = "#projects-list"
 let experience = "#experience-list"
 let aboutme = "#aboutme"
+let certificados = "#certificados"
 
 $(experience).hide();
 $(projects).hide();
+$(certificados).hide();
 
 $("#about").click(function () {
     $(aboutme).show(1000);
     $(experience).hide();
     $(projects).hide();
+    $(certificados).hide();
 });
 
 $("#experience").click(function () {
     $(aboutme).hide();
     $(experience).show(1000);
     $(projects).hide();
+    $(certificados).hide();
 });
 
 $("#projects").click(function () {
     $(aboutme).hide();
     $(experience).hide();
     $(projects).show(1000);
+    $(certificados).hide();
 });
